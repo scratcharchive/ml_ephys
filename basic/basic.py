2525a57181ec21a152eb0f75d1dc8e85fdf29399
Jeremy Magland <jeremy.magland@gmail.com>
2018-04-27 12:17:02 -0400

    add processors

--- basic/basic.py
+++ basic/basic.py
@@ -4,11 +4,13 @@ from mltools import processormanager as pm
 
 import p_compute_templates
 import p_compute_cross_correlograms
+import p_convert_array
 
 PM=pm.ProcessorManager()
 
 PM.registerProcessor(p_compute_templates.compute_templates)
 PM.registerProcessor(p_compute_cross_correlograms.compute_cross_correlograms)
+PM.registerProcessor(p_convert_array.convert_array)
 
 if not PM.run(sys.argv):
     exit(-1)
