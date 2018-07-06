060582a8632133c3ec4623b80e65ac1d0f3be05e
Tom Davidson <tjd@alum.mit.edu>
2018-07-06 06:41:33 -0700

    setup.py: add dependencies (including magland/mltools)
    Works with pip install -e . --process-dependency-links

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+pkgs = setuptools.find_packages()
+print('found these packages:', pkgs)
+
 setuptools.setup(
     name="ml_ephys",
     version="0.0.1",
@@ -12,11 +15,21 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/magland/ml_ephys",
-    packages=setuptools.find_packages(),
+    packages=pkgs,
     package_data={
         # Include all processor files
         '': ['*.mp']
     },
+    install_requires=
+    [
+        'numpy',
+        'mltools==0.1.2a',
+        'deepdish',
+        'scipy',
+        'numpydoc',
+        'h5py'
+    ],
+    dependency_links=['https://github.com/magland/mltools/tarball/master#egg=mltools-0.1.2a'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
