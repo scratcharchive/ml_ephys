f85104a5d1625b657052eeffc62614f10a1d7cd4
Jeremy Magland <jeremy.magland@gmail.com>
2018-08-01 18:28:38 -0400

    increment version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ print('found these packages:', pkgs)
 
 setuptools.setup(
     name="ml_ephys",
-    version="0.2.0",
+    version="0.2.1",
     author="Jeremy Magland",
     author_email="",
     description="ephys tools for MountainLab",
