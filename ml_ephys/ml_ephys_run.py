123ace9918f23d2ae1ae3222f499561afd601411
Witold Wysota <wysota@wysota.org>
2018-08-14 22:16:23 +0200

    Converted remaining processors

--- ml_ephys/ml_ephys_run.py
+++ ml_ephys/ml_ephys_run.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
+
 from mountainlab_pytools.mlprocessors.registry import registry, register_processor
 
 registry.namespace = "ephys_new"
 
 from validation.p_compare_ground_truth import compare_ground_truth as original_compare_ground_truth
-from mountainlab_pytools.mlprocessors.core import Input, Output, Processor, IntegerParameter, FloatParameter
+from mountainlab_pytools.mlprocessors.core import Input, Output, Processor, IntegerParameter, FloatParameter, StringParameter, IntegerListParameter
 from mountainlab_pytools.mlprocessors.validators import Validator, FileExtensionValidator
 
 class MdaInput(Input):
@@ -34,7 +36,7 @@ class MdaShapeValidatorBase(Validator):
 
 
 class MdaShapeValidator(Validator):
-    def __init__(self, *, **kwargs):
+    def __init__(self,  **kwargs):
         self.constraints = kwargs
 
     def validate(self, value):
@@ -63,6 +65,29 @@ class MdaShapeValidator(Validator):
             else:
                 raise ValueError('Dimension constraint is not defined correctly.')
 
+class MdaValidator(Validator):
+    def __init__(self, subvalidators=[]):
+        self.subvalidators = subvalidators
+
+    def add_validator(self, v):
+        self.subvalidators.append(v)
+
+    def validate(self, value):
+        mda = mdaio._read_header(value)
+        if not mda: raise ValidationError("Can't read mda header")
+        for sub in self.subvalidators:
+            if hasattr(sub, 'validate') and callable(sub.validate):
+                sub.validate(value, mda)
+            else:
+                sub(value, mda)
+
+class MdaR3Validator(MdaValidator):
+    def __init__(self):
+        def r3_validate(value, mda):
+            R = mda.shape[0]
+            if R<3: raise ValidationError("Size of the first dimension has to be at least 3")
+        super().__init__(subvalidators = [ r3_validate ])
+
 
 @register_processor(registry)
 class compare_ground_truth(Processor):
@@ -79,8 +104,8 @@ class compare_ground_truth(Processor):
 
     def run(self):
         # for now simply wrap the existing function
-        from ml_ephys.validation.p_compare_ground_truth import compare_ground_truth as original_compare_ground_truth
-        original_compare_ground_truth(
+        from validation.p_compare_ground_truth import compare_ground_truth as original_compare_ground_truth
+        return original_compare_ground_truth(
             firings_true=self.firings_true,
             firings=self.firings,
             json_out=self.json_out,
@@ -104,8 +129,8 @@ class synthesize_timeseries(Processor):
     amplitudes_row = IntegerParameter('If positive, this is the row in the firings arrays where the amplitude scale factors are found. Otherwise, use all 1\'s', optional=True, default=0)
 
     def run(self):
-        from ml_ephys.synthesis.p_synthesize_timeseries import synthesize_timeseries as original_synthesize_timeseries
-        original_synthesize_timeseries(
+        from synthesis.p_synthesize_timeseries import synthesize_timeseries as original_synthesize_timeseries
+        return original_synthesize_timeseries(
             firings=self.firings or '',
             waveforms=self.waveforms or '',
             timeseries_out=self.timeseries_out,
@@ -117,7 +142,7 @@ class synthesize_timeseries(Processor):
         )
 
     def test():
-        from ml_ephys.synthetis.p_synthetize_timeseries import test_synthetize_timeseries
+        from synthetis.p_synthetize_timeseries import test_synthetize_timeseries
         return test_synthetize_timeseries()
 
 @register_processor(registry)
@@ -130,7 +155,7 @@ class synthesize_random_firings(Processor):
 
     def run(self):
         from ml_ephys.synthesis.p_synthesize_random_firings import synthesize_random_firings as original_synthesize_random_firings
-        original_synthesize_random_firings(
+        return original_synthesize_random_firings(
             firings_out = self.firings_out,
             K = self.K,
             samplerate = self.samplerate,
@@ -140,7 +165,7 @@ class synthesize_random_firings(Processor):
     @classmethod
     def test(cls):
         print('Running test')
-        from ml_ephys.synthesis.p_synthesize_random_firings import test_synthesize_random_firings
+        from synthesis.p_synthesize_random_firings import test_synthesize_random_firings
         return test_synthesize_random_firings()
 
 @register_processor(registry)
@@ -157,7 +182,7 @@ class synthesize_random_waveforms(Processor):
 
     def run(self):
         from ml_ephys.synthesis.p_synthesize_random_waveforms import synthesize_random_waveforms as original_synthesize_random_waveforms
-        original_synthesize_random_waveforms(
+        return original_synthesize_random_waveforms(
             waveforms_out=self.waveforms_out,
             geometry_out=self.geometry_out,
             M=self.M,
@@ -169,7 +194,7 @@ class synthesize_random_waveforms(Processor):
         )
 
     def test():
-        from ml_ephys.synthesis.p_synthesize_random_waveforms import test_synthesize_random_waveforms
+        from synthesis.p_synthesize_random_waveforms import test_synthesize_random_waveforms
         return test_synthesize_random_waveforms()
 
 #@register_processor(registry)
@@ -194,8 +219,8 @@ class synthesize_drifting_timeseries(Processor):
     num_interp_nodes = IntegerParameter('For drift, the number of timepoints where we specify the waveform', optional=True, default=2)
 
     def run(self):
-        from ml_ephys.synthesis.p_synthesize_drifting_timeseries import synthesize_drifting_timeseries as original_synthesize_drifting_timeseries
-        original_synthesize_drifting_timeseries(
+        from synthesis.p_synthesize_drifting_timeseries import synthesize_drifting_timeseries as original_synthesize_drifting_timeseries
+        return original_synthesize_drifting_timeseries(
             firings = self.firings,
             waveforms = self.waveforms,
             timeseries_out = self.timeseries_out,
@@ -208,7 +233,7 @@ class synthesize_drifting_timeseries(Processor):
         )
 
     def test():
-        from ml_ephys.synthesis.p_synthesize_drifting_timeseries import test_synthesize_drifting_timeseries
+        from synthesis.p_synthesize_drifting_timeseries import test_synthesize_drifting_timeseries
         return test_synthesize_drifting_timeseries()
 
 @register_processor(registry)
@@ -226,8 +251,8 @@ class bandpass_filter(Processor):
     #num_processes=os.cpu_count()
 
     def run(self):
-        from ml_ephys.preprocessing.p_bandpass_filter import bandpass_filter as original_bandpass_filter
-        original_bandpass_filter(
+        from preprocessing.p_bandpass_filter import bandpass_filter as original_bandpass_filter
+        return original_bandpass_filter(
             timeseries     = self.timeseries,
             timeseries_out = self.timeseries_out,
             samplerate     = self.samplerate,
@@ -236,7 +261,126 @@ class bandpass_filter(Processor):
             freq_wid       = self.freq_wid
         )
 
-import sys
+@register_processor(registry)
+class compute_cluster_metrics(Processor):
+    VERSION='0.11'
+
+    firings = MdaInput(validators = [ MdaR3Validator() ])
+    timeseries = Input(optional=True)
+    metrics_out = Output()
+
+    clip_size = IntegerParameter(optional=True, minimum=0)
+    samplerate = FloatParameter(optional=True, minimum=0)
+    refrac_msec = FloatParameter(optional=True, minimum=0)
+
+    def run(self):
+        from basic.p_compute_cluster_metrics import compute_cluster_metrics as original_compute_cluster_metrics
+        return original_compute_cluster_metrics(
+            firings = self.firings,
+            timeseries = self.timeseries,
+            metrics_out = self.metrics_out,
+            clip_size = self.clip_size,
+            samplerate = self.samplerate,
+            refrac_msec = self.refrac_msec
+        )
+
+
+@register_processor(registry)
+class convert_array(Processor):
+    VERSION = '0.1'
+
+    input = Input(many=True)
+    output = Output()
+
+    format = StringParameter(optional=True)
+    format_out = StringParameter(optional=True)
+    dimensions = StringParameter(optional=True)
+    dtype = StringParameter(choices=["int8", "int16", "int32", "uint16", "uint32", "float32", "float64"])
+    dtype_out = StringParameter(choices=["int8", "int16", "int32", "uint16", "uint32", "float32", "float64"], optional=True)
+    channels = IntegerListParameter(optional=True)
+
+    def run(self):
+        from basic.p_convert_array import convert_array as original_convert_array
+        return original_convert_array(
+            input = self.input,
+            output = self.output,
+            format = self.format,
+            format_out = self.format_out,
+            dimensions = self.dimensions,
+            dtype = self.dtype,
+            dtype_out = self.dtype_out,
+            channels = self.channels
+        )
+
+#    def test():
+#        from basic.p_convert_array import test_convert_array
+#        return test_convert_array() # todo: handle params
+
+@register_processor(registry)
+class compute_cross_correlograms(Processor):
+    VERSION = '0.11'
+
+    firings = MdaInput()
+    correlograms_out = Output()
+
+    mode = StringParameter(choices=['autocorrelograms'])
+    samplerate = FloatParameter()
+    max_dt_msec = FloatParameter()
+    bin_size_msec = FloatParameter()
+
+    def run(self):
+        from basic.p_compute_cross_correlograms import compute_cross_correlograms as original_compute_cross_correlograms
+        original_compute_cross_correlograms(
+            firings = self.firings,
+            correlograms_out = self.correlograms_out,
+            mode = self.mode,
+            samplerate = self.samplerate,
+            max_dt_msec = self.max_dt_msec,
+            bin_size_msec = self.bin_size_msec
+        )
+
+@register_processor(registry)
+class compute_templates(Processor):
+    VERSION = '0.11'
+
+    timeseries = MdaInput()
+    firings = MdaInput()
+    templates_out = Output()
+    clip_size = IntegerParameter(optional=True, default=100)
+
+    def run(self):
+        p = sys.path
+        sys.path.append("basic") # otherwise can't import timeserieschunkreader
+        from basic.p_compute_templates import compute_templates as original_compute_templates
+        r = original_compute_templates(
+            timeseries = self.timeseries,
+            firings = self.firings,
+            tempates_out = self.templates_out,
+            clip_size = self.clip_size
+        )
+        sys.path = p
+        return r
+
+
+    def test():
+        p = sys.path
+        sys.path.append("basic") # otherwise can't import timeserieschunkreader
+        from basic.p_compute_templates import test_compute_templates
+        r = test_compute_templates()
+        sys.path = p
+        return r
+
+@register_processor(registry)
+class whiten(Processor):
+    VERSION='0.1'
+
+    timeseries = MdaInput()
+    timeseries_out = Output()
+
+    def run(self):
+        from preprocessing.p_whiten import whiten as original_whiten
+        original_whiten(timeseries = self.timeseries, timeseries_out = self.timeseries_out)
+
 
 if __name__ == "__main__":
     registry.process(sys.argv)
